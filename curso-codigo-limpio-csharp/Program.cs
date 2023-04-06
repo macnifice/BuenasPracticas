96824ef6b9fb6e401d53d174beeee2229e37403e
Luis Chavez <luis99ch@gmail.com>
2023-04-06 14:14:17 -0600

    Se utilizo el principio KISS, donde cambiamos el for por un for each y una expresion lamda haciendo el codigo mas sencillo

--- curso-codigo-limpio-csharp/Program.cs
+++ curso-codigo-limpio-csharp/Program.cs
@@ -57,14 +57,12 @@ namespace ToDo
                 string line = Console.ReadLine();
                 // Remove one position
                 int indexToRemove = Convert.ToInt32(line) - 1;
-                if (indexToRemove > -1)
-                {
-                    if (TaskList.Count > 0)
-                    {
+                if (indexToRemove > -1 && TaskList.Count > 0)
+                {                 
                         string task = TaskList[indexToRemove];
                         TaskList.RemoveAt(indexToRemove);
                         Console.WriteLine("Tarea " + task + " eliminada");
-                    }
+                    
                 }
             }
             catch (Exception)
@@ -95,10 +93,8 @@ namespace ToDo
             else
             {
                 Console.WriteLine("----------------------------------------");
-                for (int i = 0; i < TaskList.Count; i++)
-                {
-                    Console.WriteLine((i + 1) + ". " + TaskList[i]);
-                }
+                var indexTask = 0;
+                TaskList.ForEach(p => Console.WriteLine(++indexTask + ". " + p));
                 Console.WriteLine("----------------------------------------");
             }
         }
